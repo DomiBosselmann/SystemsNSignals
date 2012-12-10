e0203451a95750fceec1831b54973e1097667896
Dominique Bosselmann <bosselmann@co2-filter.com>
2012-12-10 17:20:31 +0100

    An Graph angepasste Simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,20 +1,34 @@
+import java.io.IOException;
+
 
 public class Simulation {
 	private double t0 = 0.0D;
 	private double tn;
 	private double t = t0;
+	private double stepSize = 0.01D;
 	
 	private Systeme theUltimateSystem;
 	private Regler theUltimateRegler;
 	
-	public Simulation (double duration, double stepSize, String[] uEquations, String[] xEquations, String[] yEquations, double setpoint, int i, int d, int p) {
+	private SimpleFunction input;
+	
+	private EingabeGUI graph;
+	
+	public Simulation (double duration, double stepSize, String[] uEquations, String[] xEquations, String[] yEquations, double setpoint, int i, int d, int p, EingabeGUI graph) {
+		this.graph = graph;
+		this.stepSize = stepSize;
+		
 		// Dat is mit Regler
 		
 		theUltimateSystem = new Systeme(xEquations, yEquations, stepSize);
 		theUltimateRegler = new Regler(theUltimateSystem, i, d, p, setpoint, stepSize);
 	}
 	
-	public Simulation (double duration, double stepSize, String[] uEquations, String[] xEquations, String[] yEquations) {
+	public Simulation (double duration, double stepSize, String[] uEquations, String[] xEquations, String[] yEquations, EingabeGUI graph) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		this.graph = graph;
+		this.stepSize = stepSize;
+		
+		this.input = MathEvaluator.convertNullOrderEquationToClass(uEquations);
 		// Und dat ohne
 		
 		theUltimateSystem = new Systeme(xEquations, yEquations, stepSize);
@@ -22,8 +36,20 @@ public class Simulation {
 	
 	private void start () {
 		while (this.t < this.tn) {
+			// NŠchsten Schritt simulieren
 			
-		}
+			if (this.theUltimateRegler != null) {
+				this.theUltimateRegler.simulateNextStep();
+			} else {
+				this.theUltimateSystem.simulateNextStep(this.t, this.input.calc(this.t));
+			}
+			
+			// Punkte der Simulation bestimmen und zeichnen lassen
+			this.graph.addPoint(this.t, this.theUltimateSystem.getCurrentValues());
+			
+			// Zeit weiter setzen
+			this.t += this.stepSize;
+		}		
 	}
 	
 }
